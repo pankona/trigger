ec0cfaf48994dc4ace1f210d39c114b24c17cae1
pankona <yosuke.akatsuka@gmail.com>
2018-07-23 23:13:13 +0900

    make them dry

--- trigger.go
+++ trigger.go
@@ -25,11 +25,32 @@ func createHttpClient(r *http.Request) *http.Client {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	circleCIBuild(w, r)
-	dockerBuild(w, r)
+	handlers := []func(w http.ResponseWriter, r *http.Request) error{
+		circleCIBuild,
+		dockerBuild,
+	}
+
+	errs := make([]error, 0)
+	w.Header().Set("", "")
+	for _, f := range handlers {
+		err := f(w, r)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		for _, v := range errs {
+			w.Write([]byte(v.Error()))
+		}
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(200)
 }
 
-func circleCIBuild(w http.ResponseWriter, r *http.Request) {
+func circleCIBuild(w http.ResponseWriter, r *http.Request) error {
 	endpoint := "https://circleci.com/api/v1/project/pankona/gomo-simra/tree/master"
 	query := url.Values{"circle-token": {getEnvVar("CIRCLECI_API_KEY")}}
 
@@ -43,16 +64,10 @@ func circleCIBuild(w http.ResponseWriter, r *http.Request) {
 
 	client := createHttpClient(r)
 	_, err := client.Do(req)
-
-	w.Header().Set("", "")
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
-	}
-	w.WriteHeader(200)
+	return err
 }
 
-func dockerBuild(w http.ResponseWriter, r *http.Request) {
+func dockerBuild(w http.ResponseWriter, r *http.Request) error {
 	endpoint := "https://registry.hub.docker.com/u/pankona/gomo-simra/trigger/" + getEnvVar("DOCKERHUB_TRIGGER_TOKEN") + "/"
 	query := url.Values{"docker_tag": {"master"}}
 
@@ -65,11 +80,5 @@ func dockerBuild(w http.ResponseWriter, r *http.Request) {
 
 	client := createHttpClient(r)
 	_, err := client.Do(req)
-
-	w.Header().Set("", "")
-	if err != nil {
-		w.Write([]byte(err.Error()))
-		w.WriteHeader(500)
-	}
-	w.WriteHeader(200)
+	return err
 }
